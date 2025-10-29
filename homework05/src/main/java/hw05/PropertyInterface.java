d045b15e0bb52943fd9d6b77464cdcffa13894ab
kareymabualfadel <100980026+kareymabualfadel@users.noreply.github.com>
2025-10-29 23:48:02 +0100

    Delete homework05 directory

--- homework05/src/main/java/hw05/PropertyInterface.java
+++
@@ -1,7 +0,0 @@
-package hw05;
-
-public interface PropertyInterface {
-    void makeDiscount(int percent);         // reduce price per sqm by percent
-    int getTotalPrice();                     // total price with city/floor/insulation rules
-    double averageSqmPerRoom();              // sqm / numberOfRooms
-}
